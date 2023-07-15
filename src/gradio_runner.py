0557cc6b4457670fd3da69d3a922310ff0d5e81e
Gunasekhar Ravilla <14607@ramco.com>
2023-07-15 23:25:08 +0530

    name change

--- src/gradio_runner.py
+++ src/gradio_runner.py
@@ -130,8 +130,8 @@ def go_gradio(**kwargs):
         instruction_label_nochat = "Text to translate"
     else:
         instruction_label_nochat = "Instruction (Shift-Enter or push Submit to send message," \
-                                   " use Enter for multiple input lines)"
-
+    title = ' ChatMate'
+    more_info = ''  # """<iframe src="https://ghbtns.com/github-btn.html?user=h2oai&repo=h2ogpt&type=star&count=true&size=small" frameborder="0" scrolling="0" width="250" height="20" title="GitHub"></iframe><small><a href="https://github.com/h2oai/h2ogpt">h2oGPT</a>  <a href="https://github.com/h2oai/h2o-llmstudio">H2O LLM Studio</a><br><a href="https://huggingface.co/h2oai">🤗 Models</a>"""
     title = 'Ramco ChatMate'
     more_info = ''  # """<iframe src="https://ghbtns.com/github-btn.html?user=h2oai&repo=h2ogpt&type=star&count=true&size=small" frameborder="0" scrolling="0" width="250" height="20" title="GitHub"></iframe><small><a href="https://github.com/h2oai/h2ogpt">h2oGPT</a>  <a href="https://github.com/h2oai/h2o-llmstudio">H2O LLM Studio</a><br><a href="https://huggingface.co/h2oai">🤗 Models</a>"""
     if kwargs['verbose']:
